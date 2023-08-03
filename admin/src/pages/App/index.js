a2ea01b193f639b8785e6da925785a5a571fd9cd
rickylabs <129366361+rickylabs@users.noreply.github.com>
2023-08-03 14:57:46 +0200

    Update index.js

--- admin/src/pages/App/index.js
+++ admin/src/pages/App/index.js
@@ -1,5 +1,5 @@
-import { NotFound } from '@strapi/helper-plugin';
 import React from 'react';
+import { AnErrorOccurred } from '@strapi/helper-plugin';
 import { Route, Switch } from 'react-router-dom';
 
 import pluginId from '../../pluginId';
@@ -10,7 +10,7 @@ const App = () => {
     <div>
       <Switch>
         <Route path={`/plugins/${pluginId}`} component={HomePage} exact />
-        <Route component={NotFound} />
+        <Route component={AnErrorOccurred} />
       </Switch>
     </div>
   );
